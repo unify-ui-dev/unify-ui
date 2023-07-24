9a614f3b6069bd83850f9807a76fcd745cd5a49c
Johnk-Mj <johnkatembue4@gmail.com>
2023-07-24 23:28:28 +0200

    add personnal portfolio hero

---
+++ ressources/hero-sections/personnal-porfolio/preview/js/unify.js
@@ -0,0 +1,27 @@
+const btnHumb = document.querySelector("[data-toggle-navbar]")
+const navbar = document.querySelector("[data-navbar]")
+const overlay = document.querySelector("[data-nav-overlay]")
+if (btnHumb && navbar) {
+    const toggleBtnAttr = () => {
+        const isOpen = btnHumb.getAttribute("data-is-open")
+        btnHumb.setAttribute("data-is-open", isOpen === "true" ? "false" : "true")
+        if (isOpen === "false") {
+            document.body.classList.toggle("overflow-y-auto")
+            overlay.classList.toggle("hidden")
+        } else {
+            document.body.classList.add("overflow-y-auto")
+            overlay.classList.add("hidden")
+        }
+    }
+    btnHumb.addEventListener("click", () => {
+        navbar.classList.toggle("invisible")
+        navbar.classList.toggle("opacity-0")
+        toggleBtnAttr()
+    })
+
+    overlay.addEventListener("click", () => {
+        navbar.classList.add("invisible")
+        navbar.classList.add("opacity-0")
+        toggleBtnAttr()
+    })
+}
