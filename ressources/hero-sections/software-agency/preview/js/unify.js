90e94cdee25bd24b98244965a4b1de78ecb152da
Johnk-Mj <johnkatembue4@gmail.com>
2023-08-04 16:04:18 +0200

    add new hero sections

---
+++ ressources/hero-sections/software-agency/preview/js/unify.js
@@ -0,0 +1,19 @@
+const btnHumb = document.querySelector("[data-toggle-navbar]")
+        const navbar = document.querySelector("[data-navbar]")
+        if (btnHumb && navbar) {
+            const toggleBtnAttr = () => {
+                const isOpen = btnHumb.getAttribute("data-is-open")
+                btnHumb.setAttribute("data-is-open", isOpen === "true" ? "false" : "true")
+                if (isOpen === "false") {
+                    document.body.classList.toggle("overflow-y-auto")
+                } else {
+                    document.body.classList.add("overflow-y-auto")
+                }
+            }
+            btnHumb.addEventListener("click", () => {
+                navbar.classList.toggle("invisible")
+                navbar.classList.toggle("op-0")
+                navbar.classList.toggle("-translate-y-10")
+                toggleBtnAttr()
+            })
+        }
