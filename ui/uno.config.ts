18b6105336a9b3d3f2dbc7bffee84ad7e9b166a1
Johnk-Mj <johnkatembue4@gmail.com>
2024-07-20 22:50:10 +0200

    feat: update UI HTML

--- ui/uno.config.ts
+++ ui/uno.config.ts
@@ -1,6 +1,5 @@
 import {
   defineConfig,
-  presetAttributify,
   presetIcons,
   presetUno,
   presetMini,
@@ -21,7 +20,20 @@ export default defineConfig({
       inherit: colors.inherit,
         current: colors.current,
         transparent: colors.transparent,
-        primary: colors.indigo,
+        primary:  {
+          DEFAULT: 'rgba(var(--c-primary-600) / <alpha-value>)',
+          50: 'rgba(var(--c-primary-50) / <alpha-value>)',
+          100: 'rgba(var(--c-primary-100) / <alpha-value>)',
+          200: 'rgba(var(--c-primary-200) / <alpha-value>)',
+          300: 'rgba(var(--c-primary-300) / <alpha-value>)',
+          400: 'rgba(var(--c-primary-400) / <alpha-value>)',
+          500: 'rgba(var(--c-primary-500) / <alpha-value>)',
+          600: 'rgba(var(--c-primary-600) / <alpha-value>)',
+          700: 'rgba(var(--c-primary-700) / <alpha-value>)',
+          800: 'rgba(var(--c-primary-800) / <alpha-value>)',
+          900: 'rgba(var(--c-primary-900) / <alpha-value>)',
+          950: 'rgba(var(--c-primary-950) / <alpha-value>)',
+        },
         secondary: colors.cyan,
         accent: colors.blue,
         danger: colors.red,
@@ -35,7 +47,6 @@ export default defineConfig({
   },
   presets: [
     presetUno(),
-    presetAttributify(),
     presetUI({}),
     presetForms({
       strategy: "base",
@@ -46,13 +57,13 @@ export default defineConfig({
     presetWebFonts({
       provider: "none",
       fonts: {
-        display: ["Oswald", "sans-serif"],
+        'display-oswald': ["Oswald", "sans-serif"],
       },
     }),
     presetIcons({
-      collections: {
-        ph: () =>
-          import("@iconify-json/ph/icons.json").then((i) => i.default),
+      extraProperties: {
+        display: 'inline-block',
+        'vertical-align': 'middle',
       },
     }),
   ],
